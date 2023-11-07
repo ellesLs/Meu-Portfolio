6148f6cc0ead91425f060746e3e44dc260b7c307
Elles do Nascimento <elles.2212@gmail.com>
2023-11-07 12:31:36 -0300

    chore: add functions to handle with clicks

--- src/pages/Projects/components/Card/CardImages/CardImages.tsx
+++ src/pages/Projects/components/Card/CardImages/CardImages.tsx
@@ -13,20 +13,23 @@ export function CardImages({ desktop, mobile, alt, hasModal }: CardImagesProps) 
   const [isOpenModalDesktop, setIsOpenModalDesketop] = useState(false);
   const [isOpenModalMobile, setIsOpenModalMobile] = useState(false);
 
+  const handleClickOpenModalDesktop = () => setIsOpenModalDesketop(!isOpenModalDesktop);
+  const handleClickOpenModalMobile = () => setIsOpenModalMobile(!isOpenModalMobile);
+
   return (
     <>
       <S.CardImgsContainer>
         <S.CardImg
           src={`/src/assets/images/projectsImgs/${desktop}.png`}
-          alt={`${alt} image desktop`}
-          onClick={() => setIsOpenModalDesketop(true)}
+          alt={`${alt} desktop image`}
+          onClick={handleClickOpenModalDesktop}
           hasModal={hasModal}
         />
         {mobile && (
           <S.CardImg
             src={`/src/assets/images/projectsImgs/${mobile}.png`}
-            alt={`${alt} image mobile`}
-            onClick={() => setIsOpenModalMobile(true)}
+            alt={`${alt} mobile image`}
+            onClick={handleClickOpenModalMobile}
             hasModal={hasModal}
           />
         )}
@@ -37,13 +40,9 @@ export function CardImages({ desktop, mobile, alt, hasModal }: CardImagesProps) 
           <Modal
             image={desktop}
             isOpen={isOpenModalDesktop}
-            setIsOpen={() => setIsOpenModalDesketop(!isOpenModalDesktop)}
+            setIsOpen={handleClickOpenModalDesktop}
           />
-          <Modal
-            image={mobile}
-            isOpen={isOpenModalMobile}
-            setIsOpen={() => setIsOpenModalMobile(!isOpenModalMobile)}
-          />
+          <Modal image={mobile} isOpen={isOpenModalMobile} setIsOpen={handleClickOpenModalMobile} />
         </>
       )}
     </>
