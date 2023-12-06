d13e983f1d64d9a73d1ad368f2a8f8a9dd54637d
Elles do Nascimento <elles.2212@gmail.com>
2023-12-06 12:58:14 -0300

    chore: Add if for custom Hook useProjectContext

--- src/Hooks/useProjectContext.ts
+++ src/Hooks/useProjectContext.ts
@@ -1,4 +1,12 @@
 import { useContext } from 'react';
 import { ProjectContext, ProjectContextProps } from '../contexts/ProjectContext';
 
-export const useProjectContext = () => useContext(ProjectContext) as ProjectContextProps;
+export const useProjectContext = () => {
+  const projectContext = useContext(ProjectContext) as ProjectContextProps;
+
+  if (!projectContext) {
+    throw new Error('useProjectContext has to be used within <ProjectContext.Provider>');
+  }
+
+  return projectContext;
+};
