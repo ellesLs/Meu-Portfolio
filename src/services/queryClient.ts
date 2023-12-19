e4f2ded201423c45be86d0082f7ac212e08c0634
Elles do Nascimento <elles.2212@gmail.com>
2023-12-19 17:13:37 -0300

    little change

--- src/services/queryClient.ts
+++ src/services/queryClient.ts
@@ -1,6 +1,6 @@
 import { QueryClient } from '@tanstack/react-query';
 
-const TIME_IN_SECONDS_TO_REFETCH = 1000 * 60; //60 secunds
+const TIME_IN_SECONDS_TO_REFETCH = 1000 * 60; //60 seconds
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
